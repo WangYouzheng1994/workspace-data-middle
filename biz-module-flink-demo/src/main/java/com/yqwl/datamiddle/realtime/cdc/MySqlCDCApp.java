5b2bb11174e1bb33705fd0d36cb21a87f017021b
muqing <2180359905@qq.com>
2022-03-04 11:11:28 +0800

    版本问题

--- biz-module-flink-demo/src/main/java/com/yqwl/datamiddle/realtime/cdc/MySqlCDCApp.java
+++ biz-module-flink-demo/src/main/java/com/yqwl/datamiddle/realtime/cdc/MySqlCDCApp.java
@@ -3,6 +3,7 @@ package com.yqwl.datamiddle.realtime.cdc;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -14,26 +15,23 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class MySqlCDCApp {
     public static void main(String[] args) throws Exception {
         MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
-                .hostname("yourHostname")
+                .hostname("192.168.3.82")
                 .port(3306)
-                .databaseList("yourDatabaseName") // set captured database
-                .tableList("yourDatabaseName.yourTableName") // set captured table
-                .username("yourUsername")
-                .password("yourPassword")
+                .databaseList("datasource_kafka") // set captured database
+                .tableList("datasource_kafka.orders", "datasource_kafka.orders_detail", "datasource_kafka.products") // set captured table
+                .username("root")
+                .password("joker")
                 .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
                 .build();
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
+        env.setParallelism(2);
         // enable checkpoint
         env.enableCheckpointing(3000);
 
-        env
-                .fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
-                // set 4 parallel source tasks
-                .setParallelism(4)
-                .print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+        DataStreamSource<String> source = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source");
 
+        source.print();
         env.execute("Print MySQL Snapshot + Binlog");
     }
 }
