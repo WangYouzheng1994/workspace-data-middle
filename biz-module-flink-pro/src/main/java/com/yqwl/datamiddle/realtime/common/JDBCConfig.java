f8d40b48490029e04bc83ba5ba8ff5024c78a328
elvira <13026512775@163.com>
2022-05-09 14:13:57 +0800

    连接数据库的sink和config

---
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/common/JDBCConfig.java
@@ -0,0 +1,12 @@
+package com.yqwl.datamiddle.realtime.common;
+
+public class JDBCConfig {
+    //URL
+    public static final String JDBC_URL = "jdbc:mysql://192.168.3.4:3306/data_middle_flink?characterEncoding=utf-8&useSSL=false";
+    //Driver
+    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    //USERNAME
+    public static final String JDBC_USERNAME = "fengqiwulian";
+    //PASSWORD
+    public static final String JDBC_PASSWORD = "fengqiwulian";
+}
