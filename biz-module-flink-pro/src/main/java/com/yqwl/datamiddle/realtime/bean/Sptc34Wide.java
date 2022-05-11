ea57ee6f52702190d14f3c675c0b031ca3f15e48
elvira <13026512775@163.com>
2022-05-11 11:31:16 +0800

    sptc34Wide  bean 修改

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/bean/Sptc34Wide.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/bean/Sptc34Wide.java
@@ -278,6 +278,60 @@ public class Sptc34Wide implements Serializable {
     private Long warehouseUpdatetime;
 
 
+    public Sptc34Wide() {
+    }
 
-
+    public Sptc34Wide(Long idnum, String vwlckdm, String vwlckmc, String czt, Integer nkr, String vsqdm, String vsxdm, String vlxr, String vdh,
+                      String vcz, String vemail, String vyddh, String vyb, String vdz, String ctybs, Long dtyrq, String vbz, String ccksx,
+                      String cglkqkw, String cccsdm, String vcftj, String cwx, String cgs, String cscfbjh, String vdzckdm, String cyssdm, String cyscdm,
+                      String vwlckjc, String cwlbm, String cwlmc, Long dtbrq, Integer batchno, String cwlbm3, String ccklx, Long dstamp, String approvalFlag,
+                      String approvalUser, Long approvalDate, String finalApprovalFlag, String finalApprovalUser, Long finalApprovalDate, String czjgsdm, String vztmcZt,
+                      String vsqsxdm, Long warehouseCreatetime, Long warehouseUpdatetime) {
+        this.idnum = idnum;
+        this.vwlckdm = vwlckdm;
+        this.vwlckmc = vwlckmc;
+        this.czt = czt;
+        this.nkr = nkr;
+        this.vsqdm = vsqdm;
+        this.vsxdm = vsxdm;
+        this.vlxr = vlxr;
+        this.vdh = vdh;
+        this.vcz = vcz;
+        this.vemail = vemail;
+        this.vyddh = vyddh;
+        this.vyb = vyb;
+        this.vdz = vdz;
+        this.ctybs = ctybs;
+        this.dtyrq = dtyrq;
+        this.vbz = vbz;
+        this.ccksx = ccksx;
+        this.cglkqkw = cglkqkw;
+        this.cccsdm = cccsdm;
+        this.vcftj = vcftj;
+        this.cwx = cwx;
+        this.cgs = cgs;
+        this.cscfbjh = cscfbjh;
+        this.vdzckdm = vdzckdm;
+        this.cyssdm = cyssdm;
+        this.cyscdm = cyscdm;
+        this.vwlckjc = vwlckjc;
+        this.cwlbm = cwlbm;
+        this.cwlmc = cwlmc;
+        this.dtbrq = dtbrq;
+        this.batchno = batchno;
+        this.cwlbm3 = cwlbm3;
+        this.ccklx = ccklx;
+        this.dstamp = dstamp;
+        this.approvalFlag = approvalFlag;
+        this.approvalUser = approvalUser;
+        this.approvalDate = approvalDate;
+        this.finalApprovalFlag = finalApprovalFlag;
+        this.finalApprovalUser = finalApprovalUser;
+        this.finalApprovalDate = finalApprovalDate;
+        this.czjgsdm = czjgsdm;
+        this.vztmcZt = vztmcZt;
+        this.vsqsxdm = vsqsxdm;
+        this.warehouseCreatetime = warehouseCreatetime;
+        this.warehouseUpdatetime = warehouseUpdatetime;
+    }
 }
