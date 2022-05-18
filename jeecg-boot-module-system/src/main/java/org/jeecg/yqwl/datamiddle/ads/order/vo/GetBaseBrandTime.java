704c5d3cc93ca02b59fe0cee87c6955a49397e43
xiaofeng1 <bdffgee@gmail.com>
2022-05-18 19:17:56 +0800

    增加vo层,作为前端接收的bean对象.

--- jeecg-boot-module-system/src/main/java/org/jeecg/yqwl/datamiddle/ads/order/vo/GetBaseBrandTime.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/yqwl/datamiddle/ads/order/vo/GetBaseBrandTime.java
@@ -1,4 +1,4 @@
-package org.jeecg.yqwl.datamiddle.ads.order.entity;
+package org.jeecg.yqwl.datamiddle.ads.order.vo;
 
 import lombok.Data;
 
@@ -15,12 +15,12 @@ public class GetBaseBrandTime {
     /**
      * 开始时间
      */
-    private String startTime;
+    private Long startTime;
 
     /**
      * 结束时间
      */
-    private String endTime;
+    private Long endTime;
 
     /**
      * 基地
