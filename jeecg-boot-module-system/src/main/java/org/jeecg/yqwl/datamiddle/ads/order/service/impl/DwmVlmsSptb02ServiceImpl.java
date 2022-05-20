16cc2b6a8520098795256abc5b77a30fc6f15302
xiaofeng1 <bdffgee@gmail.com>
2022-05-20 11:36:09 +0800

    1.新增及时率改变

--- jeecg-boot-module-system/src/main/java/org/jeecg/yqwl/datamiddle/ads/order/service/impl/DwmVlmsSptb02ServiceImpl.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/yqwl/datamiddle/ads/order/service/impl/DwmVlmsSptb02ServiceImpl.java
@@ -6,6 +6,7 @@ import org.jeecg.yqwl.datamiddle.ads.order.entity.DwmVlmsSptb02;
 import org.jeecg.yqwl.datamiddle.ads.order.vo.GetBaseBrandTime;
 import org.jeecg.yqwl.datamiddle.ads.order.mapper.DwmVlmsSptb02Mapper;
 import org.jeecg.yqwl.datamiddle.ads.order.service.IDwmVlmsSptb02Service;
+import org.jeecg.yqwl.datamiddle.ads.order.vo.TimelinessRatioVO;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -63,13 +64,20 @@ public class DwmVlmsSptb02ServiceImpl extends ServiceImpl<DwmVlmsSptb02Mapper, D
     }
 
     @Override
-    public Double findArrivalRate(GetBaseBrandTime baseBrandTime) {
+    public TimelinessRatioVO findArrivalRate(GetBaseBrandTime baseBrandTime) {
         DwmVlmsSptb02 arrivalRate = dwmVlmsSptb02Mapper.getArrivalRate(baseBrandTime);
         Double percentage = arrivalRate.getPercentage();
         //下面开始截取两位数 原数据:0.12594458438287154
         BigDecimal bigDecimal = new BigDecimal(percentage);
         Double newPercentage = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-        return newPercentage;
+        //
+        TimelinessRatioVO timelinessRatioVO = new TimelinessRatioVO();
+        timelinessRatioVO.setAllotPercent(10); //分配及时率
+        timelinessRatioVO.setOutWarehousePercent(80);//出库及时率
+        timelinessRatioVO.setStartPercent(50);//起运及时率
+        timelinessRatioVO.setHalfwayPercent(90);//在途及时率
+        timelinessRatioVO.setEndPercent(100);//到货及时率
+        return timelinessRatioVO;
     }
 
 }
