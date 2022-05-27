2625c8167cec08c9fed052c1cde8f8e9f7a5ab86
elvira <13026512775@163.com>
2022-05-27 10:53:26 +0800

    一单到底 bean DwmOneOrderToEnd 创建(数据字段后期继续完善)

---
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/bean/DwmOneOrderToEnd.java
@@ -0,0 +1,113 @@
+package com.yqwl.datamiddle.realtime.bean;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *一单到底数据宽表
+ * </p>
+ *
+ * @author yiqi
+ * @since 2022-05-27
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class DwmOneOrderToEnd implements Serializable  {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    //private Long IDNUM;
+
+    /**
+     * 车架号,底盘号
+     */
+    private String VIN;
+
+    /**
+     * 车型
+     */
+    private String VEHICLE_TYPE;
+
+    /**
+     * 品牌
+     */
+    private String BRAND;
+
+    /**
+     * cp9下线接车时间
+     */
+    private Long CP9_OFFLINE_TIME;
+
+    /**
+     * 出厂日期
+     */
+    private Long LEAVE_FACTORY_TIME;
+
+    /**
+     * 入库时间及入一汽物流基地
+     */
+    private Long IN_SITE_TIME;
+
+    /**
+     * 入库名称
+     */
+    private String IN_WAREHOUSE_NAME;
+
+    /**
+     * 计划下达时间,取sptb02.dpzrq
+     */
+    private Long PLAN_RELEASE_TIME;
+
+    /**
+     * 任务单号 取sptb02.cjsdbh
+     */
+    private String TASK_NO;
+
+    /**
+     * 配载单编号 Y号 取sptb02.cpzdbh
+     */
+    private String STOWAGE_NOTE_NO;
+
+    /**
+     * 配载单日期 取sptb02.dpzrq
+     */
+    private Long STOWAGE_NOTE_TIME;
+
+    /**
+     * 运单指派时间
+     */
+    private Long ASSIGN_TIME;
+
+    /**
+     * 承运商名称
+     */
+    private String CARRIER_NAME;
+
+    /**
+     * 实际出库时间
+     */
+    private Long ACTUAL_OUT_TIME;
+
+    /**
+     * 实际起运时间
+     */
+    private Long SHIPMENT_TIME;
+
+    /**
+     * 运输车号
+     */
+    private String TRANSPORT_VEHICLE_NO;
+
+    /**
+     * 轿车数量  ,只计算公路运输
+     */
+    private Integer VEHICLE_NUM;
+
+}
