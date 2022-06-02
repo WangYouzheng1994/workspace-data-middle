b9c7ae354acb1a8baf375cabc1d4c8623699a690
xiaofeng1 <bdffgee@gmail.com>
2022-06-02 11:12:40 +0800

    1.代码排版优化,添加相关注释

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwd/BaseStationDataAndEpcDwdApp.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwd/BaseStationDataAndEpcDwdApp.java
@@ -51,38 +51,31 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Description: 一单到底
- * @Author: muqing
- * @Date: 2022/05/06
- * @Version: V1.0
+ * @Description: <一单到底>Oracle: base_station_data与base_station_data_epc存到Mysql表
+ * @Author: XiaoFeng
+ * @Date: 2022/6/02 10:30
+ * @Version: V1.2
  */
 @Slf4j
 public class BaseStationDataAndEpcDwdApp {
 
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        conf.setString(RestOptions.BIND_PORT, "50100-50200"); // 指定访问端口
-        conf.setInteger( TaskManagerOptions.NUM_TASK_SLOTS, 10 );
-
-        //1.创建环境  Flink 流式处理环境
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
-//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
         log.info("初始化流处理环境完成");
         //设置CK相关参数
-/*        CheckpointConfig ck = env.getCheckpointConfig();
+        /* CheckpointConfig ck = env.getCheckpointConfig();
         ck.setCheckpointInterval(10000);
         ck.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
         //系统异常退出或人为Cancel掉，不删除checkpoint数据
-        ck.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);*/
+        ck.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION); */
         System.setProperty("HADOOP_USER_NAME", "root");
         log.info("checkpoint设置完成");
 
-        //kafka消费源相关参数配置
         Props props = PropertiesUtil.getProps(PropertiesUtil.ACTIVE_TYPE);
-
-/*        // kafka source2 base_station_data
+        /*   kafka消费源相关参数配置
+          // kafka source2 base_station_data
         KafkaSource<String> bsdSource = KafkaSource.<String>builder()
                 .setBootstrapServers(props.getStr("kafka.hostname"))
                 .setTopics(KafkaTopicConst.ODS_VLMS_BASE_STATION_DATA)
@@ -107,7 +100,6 @@ public class BaseStationDataAndEpcDwdApp {
         properties.put("database.serverTimezone", "UTC");
         properties.put("database.serverTimezone", "Asia/Shanghai");
         properties.put("database.url", "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(LOAD_BALANCE=YES)(FAILOVER=YES)(ADDRESS=(PROTOCOL=tcp)(HOST=" + props.getStr("cdc.oracle.hostname") + ")(PORT=1521)))(CONNECT_DATA=(SID=" + props.getStr("cdc.oracle.database") + ")))");
-
 
         //读取oracle连接配置属性
         SourceFunction<String> oracleSource = OracleSource.<String>builder()
@@ -160,7 +152,6 @@ public class BaseStationDataAndEpcDwdApp {
             }
         }).uid("filterBASE_STATION_DATA_EPC").name("filterBASE_STATION_DATA_EPC");
 
-
         //3. 进行实体类的转换
         //BASE_STATION_DATA_EPC
         SingleOutputStreamOperator<DwdBaseStationDataEpc> mapBsdEpc = filterBsdEpcDs.map(new MapFunction<String, DwdBaseStationDataEpc>() {
@@ -185,7 +176,6 @@ public class BaseStationDataAndEpcDwdApp {
             }
         }).uid("transitionBASE_STATION_DATA").name("transitionBASE_STATION_DATA");
 
-
         // 4.指定事件时间字段
         //DwdBaseStationDataEpc指定事件时间
         SingleOutputStreamOperator<DwdBaseStationDataEpc> dwdBaseStationDataEpcWithTS = mapBsdEpc.assignTimestampsAndWatermarks(
@@ -196,7 +186,7 @@ public class BaseStationDataAndEpcDwdApp {
                                 Timestamp ts = dwdBaseStationDataEpc.getTs();
                                 return ts.getTime();
                             }
-                        })).uid("assIgnDwdBaseStationDataEpcEventTime");
+                        })).uid("assIgnDwdBaseStationDataEpcEventTime").name("assIgnDwdBaseStationDataEpcEventTime");
         //DwdBaseStationData 指定事件时间
         SingleOutputStreamOperator<DwdBaseStationData> dwdBaseStationDataWithTS = mapBsd.assignTimestampsAndWatermarks(
                 WatermarkStrategy.<DwdBaseStationData>forBoundedOutOfOrderness(Duration.ofMinutes(5))
@@ -206,16 +196,13 @@ public class BaseStationDataAndEpcDwdApp {
                                 Timestamp ts = dwdBaseStationData.getTs();
                                 return ts.getTime();
                             }
-                        })).uid("assIgnDwdBaseStationDataEventTime");
-
+                        })).uid("assIgnDwdBaseStationDataEventTime").name("assIgnDwdBaseStationDataEpcEventTime");
 
         //5.分组指定关联key,base_station_data_epc 处理CP9下线接车日期
         SingleOutputStreamOperator<DwdBaseStationDataEpc> map = dwdBaseStationDataEpcWithTS.keyBy(DwdBaseStationDataEpc::getVIN).map(new CP9Station());
-//         KeyedStream<DwdBaseStationData, String> dwdBdsKeyedStream = dwdBaseStationDataWithTS.keyBy(DwdBaseStationData::getVIN);
-
 
         //6.处理字段 base_station_data 和rfid_warehouse关联添加入库仓库的字段
-//        provincesWideWithSysc09.assignTimestampsAndWatermarks(WatermarkStrategy.forMonotonousTimestamps());
+        // provincesWideWithSysc09.assignTimestampsAndWatermarks(WatermarkStrategy.forMonotonousTimestamps());
         SingleOutputStreamOperator<DwdBaseStationData> outSingleOutputStreamOperator = AsyncDataStream.unorderedWait(
                 dwdBaseStationDataWithTS,
                 new DimAsyncFunction<DwdBaseStationData>(DimUtil.MYSQL_DB_TYPE, "ods_vlms_rfid_warehouse", "WAREHOUSE_CODE") {
@@ -223,21 +210,18 @@ public class BaseStationDataAndEpcDwdApp {
                     public Object getKey(DwdBaseStationData dwdBsd) {
                         if (StringUtils.isNotEmpty(dwdBsd.getSHOP_NO())){
                             String shop_no = dwdBsd.getSHOP_NO();
-//                            log.info("sql查询:"+shop_no);
                             return shop_no;
                         }
-                        return null;
+                        return "此条sql无SHOP_NO";
                     }
                     @Override
                     public void join(DwdBaseStationData dBsd, JSONObject dimInfoJsonObj) {
                         if (dimInfoJsonObj.getString("WAREHOUSE_CODE") !=null){
-//                            log.info("dim WAREHOUSE_CODE");
                             dBsd.setIN_WAREHOUSE_CODE(dimInfoJsonObj.getString("WAREHOUSE_CODE"));
                             dBsd.setIN_WAREHOUSE_NAME(dimInfoJsonObj.getString("WAREHOUSE_NAME"));
                         }
                     }
                 }, 60, TimeUnit.SECONDS).uid("base+rfid");
-//       outSingleOutputStreamOperator.print("处理字段后的:");
 
         //7.开窗,按照时间窗口存储到mysql
         outSingleOutputStreamOperator.assignTimestampsAndWatermarks(WatermarkStrategy.forMonotonousTimestamps());
@@ -247,7 +231,6 @@ public class BaseStationDataAndEpcDwdApp {
                 ArrayList<DwdBaseStationData> es = Lists.newArrayList(iterable);
                 log.info("插入sql");
                 if (es.size() > 0) {
-                    log.info("size>0后:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
                     collector.collect(es);
                 }
             }
@@ -287,33 +270,20 @@ public class BaseStationDataAndEpcDwdApp {
         @Override
         public DwdBaseStationDataEpc map(DwdBaseStationDataEpc dwdBaseStationDataEpc) throws Exception {
             /**
-             * 1.当前逻辑以第一次下线时间和DwdBaseStationDataEpc的bean存在状态后端,每次操作都会经过map,所以就会导致每次的更新都会经过这个逻辑
-             *
+             * 1.当前状态后端的状态为Map类型,key为String,也就是汽车的Vin码,value为vin码所对应的数据,也就是dwdBaseStationDataEpc对象
+             * 2.每次流到了到了这里,就会调用这里的map:
              */
             String vin = dwdBaseStationDataEpc.getVIN();                    //车架号
-            String operator = dwdBaseStationDataEpc.getOPERATOR();          //操作员
             Long nowOperatetime = dwdBaseStationDataEpc.getOPERATETIME();   //操作时间
-            String epc = dwdBaseStationDataEpc.getEPC();                    //epc码
-            String opertype = dwdBaseStationDataEpc.getOPERTYPE();          //操作类型 1001新增 1002更新
-            String cp = dwdBaseStationDataEpc.getCP();                      //工厂下线口
-            String shopno = dwdBaseStationDataEpc.getSHOPNO();              //站点编号
-            Long insert_date = dwdBaseStationDataEpc.getINSERT_DATE();      //增加时间
-
+            // 1):判断状态后端有无当前数据的vin码的key所对应的对象,没有就添加上
             if (myMapState.get(vin)==null){
-//                DwdBaseStationDataEpc dataEpc = new DwdBaseStationDataEpc();
-//                dataEpc.setVIN(vin);
-//                dataEpc.setEPC(operator);
-//                dataEpc.setOPERATETIME(nowOperatetime);
-//                dataEpc.setEPC(epc);
-//                dataEpc.setOPERTYPE(opertype);
-//                dataEpc.setCP(cp);
-//                dataEpc.setSHOPNO(shopno);
-//                dataEpc.setINSERT_DATE(insert_date);
-
                 myMapState.put(vin,dwdBaseStationDataEpc);
                 dwdBaseStationDataEpc.setCP9_OFFLINE_TIME(nowOperatetime);
                 return dwdBaseStationDataEpc;
             }else {
+            // 2):当前'状态后端'有vin码对应的value就会判断操作时间,
+            //    如果'状态后端'已有的操作时间大于'当前流数据'的操作时间则删除'状态后端'中的key(因为取的是第一条下线时间的数据).
+            //    然后再把更早的下线时间的对象存到'状态后端'中.
                 DwdBaseStationDataEpc oldDataEpc = myMapState.get(vin);
                 Long oldOperatetime = oldDataEpc.getOPERATETIME();
                 if (oldOperatetime>nowOperatetime){
@@ -322,6 +292,7 @@ public class BaseStationDataAndEpcDwdApp {
                     dwdBaseStationDataEpc.setCP9_OFFLINE_TIME(nowOperatetime);
                     return dwdBaseStationDataEpc;
                 }else {
+            // 3):如果'状态后端'已有的操作时间小于当前流的操作时间,就会保留当前状态后端的操作时间,且设置为DwdBaseStationDataEpc的第一次下线时间.
                     oldDataEpc.setCP9_OFFLINE_TIME(oldOperatetime);
                     return oldDataEpc;
                 }
