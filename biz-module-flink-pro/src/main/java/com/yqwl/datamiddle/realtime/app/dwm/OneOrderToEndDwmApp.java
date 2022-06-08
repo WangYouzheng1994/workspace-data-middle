22b04918e6f6e2edaf279859f47bd7289286562d
muqing <2180359905@qq.com>
2022-06-08 13:05:42 +0800

    修改

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/OneOrderToEndDwmApp.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/OneOrderToEndDwmApp.java
@@ -6,9 +6,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.yqwl.datamiddle.realtime.app.func.DimAsyncFunction;
-import com.yqwl.datamiddle.realtime.app.func.JdbcSink;
 import com.yqwl.datamiddle.realtime.bean.*;
 import com.yqwl.datamiddle.realtime.common.KafkaTopicConst;
+import com.yqwl.datamiddle.realtime.common.MysqlConfig;
 import com.yqwl.datamiddle.realtime.util.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -17,6 +17,9 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.RichFilterFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.AsyncDataStream;
@@ -74,7 +77,7 @@ public class OneOrderToEndDwmApp {
                 .build();
         //1.将mysql中的源数据转化成 DataStream
         SingleOutputStreamOperator<String> mysqlSource = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MysqlSource").uid("MysqlSourceStream").name("MysqlSourceStream");
-
+        //==============================================dwm_vlms_sptb02处理====================================================================//
         //2.进行数据过滤
         SingleOutputStreamOperator<String> filterSptb02 = mysqlSource.filter(new RichFilterFunction<String>() {
             @Override
@@ -92,40 +95,6 @@ public class OneOrderToEndDwmApp {
             }
         });
 
-        // 过滤出BASE_STATION_DATA的表
-        DataStream<String> filterBsdDs = mysqlSource.filter(new RichFilterFunction<String>() {
-            @Override
-            public boolean filter(String mysqlDataStream) throws Exception {
-                JSONObject jo = JSON.parseObject(mysqlDataStream);
-                if (jo.getString("database").equals("data_middle_flink") && jo.getString("tableName").equals("dwd_vlms_base_station_data")) {
-                    DwdBaseStationData after = jo.getObject("after", DwdBaseStationData.class);
-                    String vin = after.getVIN();
-                    if (vin != null) {
-                        return true;
-                    }
-                    return false;
-                }
-                return false;
-            }
-        }).uid("filterDwd_vlms_base_station_data").name("filterDwd_vlms_base_station_data");
-
-        // 过滤出BASE_STATION_DATA_Epc的表
-        DataStream<String> filterBsdEpcDs = mysqlSource.filter(new RichFilterFunction<String>() {
-            @Override
-            public boolean filter(String mysqlDataStream) throws Exception {
-                JSONObject jo = JSON.parseObject(mysqlDataStream);
-                if (jo.getString("database").equals("data_middle_flink") && jo.getString("tableName").equals("dwd_vlms_base_station_data_epc")) {
-                    DwdBaseStationDataEpc after = jo.getObject("after", DwdBaseStationDataEpc.class);
-                    String vin = after.getVIN();
-                    if (vin != null) {
-                        return true;
-                    }
-                    return false;
-                }
-                return false;
-            }
-        }).uid("filterDwd_vlms_base_station_data_epc").name("filterDwd_vlms_base_station_data_epc");
-        
         //3.进行实体类转换
         //转换sptb02为实体类
         SingleOutputStreamOperator<OotdTransition> mapOotdTransition = filterSptb02.map(new MapFunction<String, OotdTransition>() {
@@ -149,54 +118,79 @@ public class OneOrderToEndDwmApp {
                 String start_city_name = dwmSptb02.getSTART_CITY_NAME();//始发城市
                 String end_city_name = dwmSptb02.getEND_CITY_NAME();    //目的城市
                 String dealer_name = dwmSptb02.getDEALER_NAME();        //经销商代码(名称)
-                if (StringUtils.isNotBlank(cjsdbh)){
+                if (StringUtils.isNotBlank(cjsdbh)) {
                     ootdTransition.setCJSDBH(cjsdbh);
-                if (StringUtils.isNotBlank(vehicle_code)) {
-                    ootdTransition.setVEHICLE_CODE(vehicle_code);
-                }
-                if (StringUtils.isNotBlank(vvin)) {
-                    ootdTransition.setVVIN(vvin);
-                }
-                if (ddjrq != null) {
-                    ootdTransition.setDDJRQ(ddjrq);
-                }
-                if (StringUtils.isNotBlank(cjhdh)) {
-                    ootdTransition.setCJHDH(cjhdh);
-                }
-                if (dpzrq != null) {
-                    ootdTransition.setDPZRQ(dpzrq);
-                }
-                if (StringUtils.isNotBlank(cpzdbh)) {
-                    ootdTransition.setCPZDBH(cpzdbh);
-                }
-                if (assign_time != null) {
-                    ootdTransition.setASSIGN_TIME(assign_time);
-                }
-                if (StringUtils.isNotBlank(ccysdm)) {
-                    ootdTransition.setASSIGN_NAME(ccysdm);
-                }
-                if (actual_out_time != null) {
-                    ootdTransition.setACTUAL_OUT_TIME(actual_out_time);
-                }
-                if (shipment_time != null) {
-                    ootdTransition.setSHIPMENT_TIME(shipment_time);
-                }
-                if (StringUtils.isNotBlank(vjsydm)) {
-                    ootdTransition.setVJSYDM(vjsydm);
-                }
-                if (StringUtils.isNotBlank(start_city_name)) {
-                    ootdTransition.setSTART_CITY_NAME(start_city_name);
-                }
-                if (StringUtils.isNotBlank(end_city_name)) {
-                    ootdTransition.setEND_CITY_NAME(end_city_name);
-                }
-                if (StringUtils.isNotBlank(dealer_name)) {
-                    ootdTransition.setDEALER_NAME(dealer_name);
-                }
+                    if (StringUtils.isNotBlank(vehicle_code)) {
+                        ootdTransition.setVEHICLE_CODE(vehicle_code);
+                    }
+                    if (StringUtils.isNotBlank(vvin)) {
+                        ootdTransition.setVVIN(vvin);
+                    }
+                    if (ddjrq != null) {
+                        ootdTransition.setDDJRQ(ddjrq);
+                    }
+                    if (StringUtils.isNotBlank(cjhdh)) {
+                        ootdTransition.setCJHDH(cjhdh);
+                    }
+                    if (dpzrq != null) {
+                        ootdTransition.setDPZRQ(dpzrq);
+                    }
+                    if (StringUtils.isNotBlank(cpzdbh)) {
+                        ootdTransition.setCPZDBH(cpzdbh);
+                    }
+                    if (assign_time != null) {
+                        ootdTransition.setASSIGN_TIME(assign_time);
+                    }
+                    if (StringUtils.isNotBlank(ccysdm)) {
+                        ootdTransition.setASSIGN_NAME(ccysdm);
+                    }
+                    if (actual_out_time != null) {
+                        ootdTransition.setACTUAL_OUT_TIME(actual_out_time);
+                    }
+                    if (shipment_time != null) {
+                        ootdTransition.setSHIPMENT_TIME(shipment_time);
+                    }
+                    if (StringUtils.isNotBlank(vjsydm)) {
+                        ootdTransition.setVJSYDM(vjsydm);
+                    }
+                    if (StringUtils.isNotBlank(start_city_name)) {
+                        ootdTransition.setSTART_CITY_NAME(start_city_name);
+                    }
+                    if (StringUtils.isNotBlank(end_city_name)) {
+                        ootdTransition.setEND_CITY_NAME(end_city_name);
+                    }
+                    if (StringUtils.isNotBlank(dealer_name)) {
+                        ootdTransition.setDEALER_NAME(dealer_name);
+                    }
                 }
                 return ootdTransition;
             }
         });
+
+        //4.根据车型代码,查出车辆名称
+        SingleOutputStreamOperator<OotdTransition> ootdAddCarNameStream = AsyncDataStream.unorderedWait(mapOotdTransition,
+                new DimAsyncFunction<OotdTransition>(DimUtil.MYSQL_DB_TYPE, "ods_vlms_mdac12", "CCPDM") {
+                    @Override
+                    public Object getKey(OotdTransition ootd) {
+                        if (StringUtils.isNotBlank(ootd.getVEHICLE_CODE())) {
+                            String vehicle_code = ootd.getVEHICLE_CODE();
+                            return vehicle_code;
+                        }
+                        return "此条sql无vehicle_code";
+                    }
+
+                    @Override
+                    public void join(OotdTransition ootd, JSONObject dimInfoJsonObj) throws Exception {
+                        if (dimInfoJsonObj.getString("CCPDM") != null) {
+                            ootd.setVEHICLE_NMAE(dimInfoJsonObj.getString("VCPMC"));
+                        }
+                    }
+                }, 60, TimeUnit.SECONDS).uid("base+VEHICLE_NMAE").name("base+VEHICLE_NMAE");
+//        ootdAddCarNameStream.print("合并了车的流:");
+
+        //filterBsdDs.print("bSD:");
+
+
 //{"database":"data_middle_flink","before":{},
 // "after":{"SAMPLE_STATUS":0,"STATION_CODE":"K","IN_WAREHOUSE_NAME":"照宝库","LAST_UPDATE_DATE":1652206207000000,
 // "EFFECT_FLAG":"1","IDNUM":9513,"BRAND_CODE":"LFPH","SAMPLE_U_T_C":1652205865000000,
@@ -205,6 +199,26 @@ public class OneOrderToEndDwmApp {
 // "IN_WAREHOUSE_CODE":"K","SHOP_NO":"K","OPERATE_TYPE":"InStock","OPERATOR_ID":"805","BRAND_NAME":"红旗",
 // "VIN":"LFPHC7CE4N2A04481","FILE_TYPE":0,"MOTORCYCLETYPE_CODE":"EQM5","STATION_TYPE":"TypeD","PUSH_BATCH":"9f62fb8f-352b-4768-8798-b83bd85c3074"},
 // "type":"insert","tableName":"dwd_vlms_base_station_data","ts":1654614547474}
+
+
+        //==============================================dwd_base_station_data处理====================================================================//
+        // 过滤出BASE_STATION_DATA的表
+        DataStream<String> filterBsdDs = mysqlSource.filter(new RichFilterFunction<String>() {
+            @Override
+            public boolean filter(String mysqlDataStream) throws Exception {
+                JSONObject jo = JSON.parseObject(mysqlDataStream);
+                if (jo.getString("database").equals("data_middle_flink") && jo.getString("tableName").equals("dwd_vlms_base_station_data")) {
+                    DwdBaseStationData after = jo.getObject("after", DwdBaseStationData.class);
+                    String vin = after.getVIN();
+                    if (vin != null) {
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+            }
+        }).uid("filterDwd_vlms_base_station_data").name("filterDwd_vlms_base_station_data");
+
         //转换BASE_STATION_DATA为实体类
         SingleOutputStreamOperator<DwdBaseStationData> mapBsd = filterBsdDs.map(new MapFunction<String, DwdBaseStationData>() {
             @Override
@@ -218,6 +232,27 @@ public class OneOrderToEndDwmApp {
                 return dataBsd;
             }
         }).uid("transitionBASE_STATION_DATA").name("transitionBASE_STATION_DATA");
+
+
+        //==============================================dwd_base_station_data_epc处理====================================================================//
+        // 过滤出BASE_STATION_DATA_Epc的表
+        DataStream<String> filterBsdEpcDs = mysqlSource.filter(new RichFilterFunction<String>() {
+            @Override
+            public boolean filter(String mysqlDataStream) throws Exception {
+                JSONObject jo = JSON.parseObject(mysqlDataStream);
+                if (jo.getString("database").equals("data_middle_flink") && jo.getString("tableName").equals("dwd_vlms_base_station_data_epc")) {
+                    DwdBaseStationDataEpc after = jo.getObject("after", DwdBaseStationDataEpc.class);
+                    String vin = after.getVIN();
+                    if (vin != null) {
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+            }
+        }).uid("filterDwd_vlms_base_station_data_epc").name("filterDwd_vlms_base_station_data_epc");
+
+        filterBsdEpcDs.print("epcFilter数据输出:");
         //BASE_STATION_DATA_EPC
         SingleOutputStreamOperator<DwdBaseStationDataEpc> mapBsdEpc = filterBsdEpcDs.map(new MapFunction<String, DwdBaseStationDataEpc>() {
             @Override
@@ -230,43 +265,36 @@ public class OneOrderToEndDwmApp {
             }
         }).uid("transitionBASE_STATION_DATA_EPCMap").name("transitionBASE_STATION_DATA_EPCMap");
 
-        //4.根据车型代码,查出车辆名称
-        SingleOutputStreamOperator<OotdTransition> ootdAddCarNameStream = AsyncDataStream.unorderedWait(mapOotdTransition,
-                new DimAsyncFunction<OotdTransition>(DimUtil.MYSQL_DB_TYPE, "ods_vlms_mdac12", "CCPDM") {
-                    @Override
-                    public Object getKey(OotdTransition ootd) {
-                        if (StringUtils.isNotBlank(ootd.getVEHICLE_CODE())) {
-                            String vehicle_code = ootd.getVEHICLE_CODE();
-                            return vehicle_code;
-                        }
-                        return "此条sql无vehicle_code";
-                    }
-                    @Override
-                    public void join(OotdTransition ootd, JSONObject dimInfoJsonObj) throws Exception {
-                        if (dimInfoJsonObj.getString("CCPDM") != null) {
-                            ootd.setVEHICLE_NMAE(dimInfoJsonObj.getString("VCPMC"));
-                        }
-                    }
-                }, 60, TimeUnit.SECONDS).uid("base+VEHICLE_NMAE").name("base+VEHICLE_NMAE");
-//        ootdAddCarNameStream.print("合并了车的流:");
+        mapBsdEpc.print("epc数据输出:");
+        mapBsdEpc.addSink(JdbcSink.sink(
 
-        filterBsdDs.print("bSD:");
-        //5.插入mysql
-
-//        filterBsdDs  Map {"values:" {name:1, age: 2}, "where" : ""}
-        filterBsdDs.map(new MapFunction<String, Map>() {
-            @Override
-            public Map map(String bsdDsvalue) throws Exception {
-//                Map<String, Object> objectObjectHashMap = new HashMap<>();
-                List<String> list = new ArrayList<>();
-                JSONObject bsdJson = JSON.parseObject(bsdDsvalue);
-//                bsdJson.getObject("after",)
-//                jsonObject.getString()
-//                list.add()
-                return null;
-            }
-        });
-//        filterBsdDs.addSink(MysqlUtil.insertOnDUMPLICATEKEYSql(""));
+                "INSERT INTO dwm_vlms_one_order_to_end (VIN, CP9_OFFLINE_TIME, BASE_NAME, BASE_CODE )\n" +
+                        "VALUES\n" +
+                        "        (?, ?, ?, ? ) \n"
+                     /*   "        ON DUMPLICATE KEY UPDATE \n" +
+                        "  VIN=?, CP9_OFFLINE_TIME=? ,BASE_NAME=?,\n" +
+                        "        BASE_CODE=?;"*/,
+                (ps, epc) -> {
+                    ps.setString(1, epc.getVIN());
+                    ps.setLong(2, epc.getCP9_OFFLINE_TIME());
+                    ps.setString(3, epc.getBASE_NAME());
+                    ps.setString(4, epc.getBASE_CODE());
+              /*      ps.setString(5, epc.getVIN());
+                    ps.setLong(6, epc.getCP9_OFFLINE_TIME());
+                    ps.setString(7, epc.getBASE_NAME());
+                    ps.setString(8, epc.getBASE_CODE());*/
+                },
+                new JdbcExecutionOptions.Builder()
+                        .withBatchSize(5000)
+                        .withBatchIntervalMs(5000L)
+                        .withMaxRetries(2)
+                        .build(),
+                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                        .withUrl(MysqlConfig.URL)
+                        .withDriverName(MysqlConfig.DRIVER)
+                        .withUsername(MysqlConfig.USERNAME)
+                        .withPassword(MysqlConfig.PASSWORD)
+                        .build())).uid("baseStationDataEpcSink").name("baseStationDataEpcSink");
 
         env.execute("合表开始");
         log.info("base_station_data job任务开始执行");
