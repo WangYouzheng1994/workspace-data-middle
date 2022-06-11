d6c1dc3d919654f4aaea1583e87d8283564c85fd
muqing <2180359905@qq.com>
2022-06-11 14:05:44 +0800

    修复sql错误

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/func/SimpleBaseStationDataSink.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/func/SimpleBaseStationDataSink.java
@@ -28,26 +28,26 @@ public class SimpleBaseStationDataSink<T> extends RichSinkFunction<DwdBaseStatio
         //当前查询到记录值为空，从来没有被更新过
         if (IN_STORE.equals(operateType)) {
             String inSql = "UPDATE dwm_vlms_sptb02 SET IN_START_PLATFORM_TIME=" + data.getSAMPLE_U_T_C() + " WHERE VYSFS IN ('L1', 'T') AND START_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_START_PLATFORM_TIME == 0 OR IN_START_PLATFORM_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_START_PLATFORM_TIME = 0 OR IN_START_PLATFORM_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
         }
         // 1.2 处理开始站台的出站台时间
         if (OUT_STOCK.equals(operateType)) {
             String inSql = "UPDATE dwm_vlms_sptb02 SET OUT_START_PLATFORM_TIME=" + data.getSAMPLE_U_T_C() + " WHERE VYSFS IN ('L1', 'T') AND START_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( OUT_START_PLATFORM_TIME == 0 OR OUT_START_PLATFORM_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( OUT_START_PLATFORM_TIME = 0 OR OUT_START_PLATFORM_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
         }
 
         //1.3 处理目的站台的入站台时间和出站台时间
         if (IN_STORE.equals(operateType)) {
             String inSql = "UPDATE dwm_vlms_sptb02 SET IN_END_PLATFORM_TIME=" + data.getSAMPLE_U_T_C() + " WHERE VYSFS IN ('L1', 'T') AND END_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_END_PLATFORM_TIME == 0 OR IN_END_PLATFORM_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_END_PLATFORM_TIME = 0 OR IN_END_PLATFORM_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
         }
         //处理出目的站台时间
         if (OUT_STOCK.equals(operateType)) {
             String inSql = "UPDATE dwm_vlms_sptb02 SET UNLOAD_RAILWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE VYSFS in ('L1', 'T') AND END_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( UNLOAD_RAILWAY_TIME == 0 OR UNLOAD_RAILWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( UNLOAD_RAILWAY_TIME = 0 OR UNLOAD_RAILWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
         }
 
@@ -57,26 +57,26 @@ public class SimpleBaseStationDataSink<T> extends RichSinkFunction<DwdBaseStatio
         if (IN_STORE.equals(operateType)) {
 
             String inSql = "UPDATE dwm_vlms_sptb02 SET IN_START_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND START_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_START_WATERWAY_TIME == 0 OR IN_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_START_WATERWAY_TIME = 0 OR IN_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
         }
         // 1.2 处理开始站台的出站台时间
         if (OUT_STOCK.equals(operateType)) {
             String inSql = "UPDATE dwm_vlms_sptb02 SET END_START_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND START_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( END_START_WATERWAY_TIME == 0 OR END_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( END_START_WATERWAY_TIME = 0 OR END_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
         }
 
         //1.3 处理目的站台的入站台时间和出站台时间
         if (IN_STORE.equals(operateType)) {
             String inSql = "UPDATE dwm_vlms_sptb02 SET IN_END_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND END_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_END_WATERWAY_TIME == 0 OR IN_END_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( IN_END_WATERWAY_TIME = 0 OR IN_END_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
         }
         //处理出目的站台时间
         if (OUT_STOCK.equals(operateType)) {
             String inSql = "UPDATE dwm_vlms_sptb02 SET UNLOAD_SHIP_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND END_WAREHOUSE_CODE='"
-                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( UNLOAD_SHIP_TIME == 0 OR UNLOAD_SHIP_TIME > " + data.getSAMPLE_U_T_C() + " )";
+                    + data.getIN_WAREHOUSE_CODE() + "' AND VVIN='" + data.getVIN() + "' AND ( UNLOAD_SHIP_TIME = 0 OR UNLOAD_SHIP_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql);
 
         }
