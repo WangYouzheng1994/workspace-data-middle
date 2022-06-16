23baf76e4444fb2d248f4b06e90733f9e81746ff
xiaofeng1 <bdffgee@gmail.com>
2022-06-16 23:08:38 +0800

    springboot batch: init druid datasource config from Spring Ioc. By Qingsong.

--- jeecg-boot-module-system/src/main/java/org/jeecg/yqwl/datamiddle/job/common/MysqlConfig.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/yqwl/datamiddle/job/common/MysqlConfig.java
@@ -1,14 +1,35 @@
 package org.jeecg.yqwl.datamiddle.job.common;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 /**
  * mysql 相关配置
  */
+@Component
 public class MysqlConfig {
     //Driver
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://192.168.3.4:3306/data_middle_flink?characterEncoding=utf8&serverTimezone=UTC&useSSL=false&rewriteBatchedStatements=true";
-    public static final String USERNAME = "fengqiwulian";
-    public static final String PASSWORD = "fengqiwulian";
+
+    public static String URL;
+    @Value("${spring.datasource.dynamic.datasource.wareHouse.url}")
+    public static String USERNAME = "fengqiwulian";
+    public static String PASSWORD = "fengqiwulian";
+
+    @Value("${spring.datasource.dynamic.datasource.wareHouse.url}")
+    public void setURL(String url) {
+        MysqlConfig.URL = url;
+    }
+
+    @Value("${spring.datasource.dynamic.datasource.wareHouse.username}")
+    public void setUsername(String username) {
+        MysqlConfig.USERNAME = username;
+    }
+
+    @Value("${spring.datasource.dynamic.datasource.wareHouse.password}")
+    public void setPassword(String password) {
+        MysqlConfig.PASSWORD = password;
+    }
 
     // serverTimezone=UTC
 //    public static final String URL = "jdbc:mysql://10.123.175.197:3306/data_flink?characterEncoding=utf8&serverTimezone=UTC&useSSL=false&rewriteBatchedStatements=true";
