2614c61e40f443fdd170e83f1e82818018ae5119
xiaofeng1 <bdffgee@gmail.com>
2022-07-06 00:11:27 +0800

    <一单到底>xiaofeng fix 更新入库仓库,代码 两个sql合一个

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/func/SimpleBsdSinkOOTD.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/func/SimpleBsdSinkOOTD.java
@@ -31,37 +31,34 @@ public class SimpleBsdSinkOOTD<T> extends RichSinkFunction<DwdBaseStationData> {
         String operate_type = dbsData.getOPERATE_TYPE();
         // 获取Shop_No匹配ware_hosue_code
         String shop_no = dbsData.getSHOP_NO();
+        System.out.println(shop_no);
 
         StringBuilder sb = new StringBuilder();
         //-------------------------------------处理更新Mysql----------------------------------//
         // 1.插入mysql 更新IN_WAREHOUSE_NAME，IN_WAREHOUSE_CODE 仓库代码,仓库名称
-        if (StringUtils.isNotBlank(vin)) {
-            if (StringUtils.isNotBlank(in_warehouse_code)) {
-                /* sb.append("UPDATE dwm_vlms_one_order_to_end SET IN_WAREHOUSE_NAME = '" + in_warehouse_name + "' , IN_WAREHOUSE_CODE= '"
-                        + in_warehouse_code + "' , WAREHOUSE_UPDATETIME = " + nowTime + " WHERE VIN = '" + vin + "' ;"); */
-
-                    // TODO: 2.更新基地入库时间/入库仓库名称,代码
-                if (sample_u_t_c != null) {
-                    /* sb.append("UPDATE dwm_vlms_one_order_to_end e JOIN dim_vlms_warehouse_rs a on e.IN_WAREHOUSE_CODE = a.WAREHOUSE_CODE SET e.IN_SITE_TIME = " + sample_u_t_c +
-                            " , e.WAREHOUSE_UPDATETIME = " + nowTime + " WHERE e.VIN = '" + vin + "'  AND e.LEAVE_FACTORY_TIME < " + sample_u_t_c + " AND a.WAREHOUSE_TYPE = 'T1' "
-                            + "AND (e.IN_SITE_TIME > " + sample_u_t_c + " or e.IN_SITE_TIME = 0);");
- */
-                    if (StringUtils.equals(operate_type, "InStock")) {
-                        // 3.更新末端配送入库时间
-                        sb.append("UPDATE dwm_vlms_one_order_to_end e JOIN dim_vlms_warehouse_rs a on a.WAREHOUSE_CODE = " + in_warehouse_code +
-                                "JOIN dwm_vlms_sptb02 s on e.VIN = s.VVIN SET e.IN_DISTRIBUTE_TIME = " + sample_u_t_c +
-                                " , e.WAREHOUSE_UPDATETIME = " + nowTime + " WHERE e.VIN = '" + vin + "'  AND e.LEAVE_FACTORY_TIME < " + sample_u_t_c + " AND a.WAREHOUSE_TYPE = 'T2' AND s.VYSFS = 'G' "
-                                + "AND e.IN_SITE_TIME < " + sample_u_t_c + ";");
-                    }
-                }
-            }
-            // 4.过滤出所有出库操作记录
+        if (StringUtils.isNotBlank(vin) && sample_u_t_c != null ) {
+            // 1.过滤出所有出库操作记录
             if (StringUtils.equals(operate_type, "OutStock") && (StringUtils.equals(shop_no, "DZCP901") || StringUtils.equals(shop_no, "DZCP9"))) {
-                // 5.更新出厂日期
+                // 1.1.更新出厂日期
                 sb.append("UPDATE dwm_vlms_one_order_to_end e  SET e.LEAVE_FACTORY_TIME = " + sample_u_t_c +
                         " , e.WAREHOUSE_UPDATETIME = " + nowTime + " WHERE e.VIN = '" + vin + "'  AND e.CP9_OFFLINE_TIME < " + sample_u_t_c +
                         " AND ( e.LEAVE_FACTORY_TIME = 0 OR e.LEAVE_FACTORY_TIME > " + sample_u_t_c + ");");
             }
+            if (StringUtils.isNotBlank(in_warehouse_code) ) {
+                    // 2.更新入库日期,入库仓库名称,入库仓库代码,更新更新时间
+                     sb.append("UPDATE dwm_vlms_one_order_to_end e JOIN dim_vlms_warehouse_rs a on e.IN_WAREHOUSE_CODE = a.WAREHOUSE_CODE SET e.IN_WAREHOUSE_NAME= '" +in_warehouse_name+ "' , e.IN_WAREHOUSE_CODE= '"
+                             + in_warehouse_code + "', e.IN_SITE_TIME = " + sample_u_t_c +
+                            " , e.WAREHOUSE_UPDATETIME = " + nowTime + "  WHERE e.VIN = '" + vin + "'  AND e.LEAVE_FACTORY_TIME < " + sample_u_t_c + " AND a.WAREHOUSE_TYPE = 'T1' "
+                            + "AND (e.IN_SITE_TIME > " + sample_u_t_c + " or e.IN_SITE_TIME = 0);");
+                    if (StringUtils.equals(operate_type, "InStock")) {
+                        // 3.更新末端配送入库时间
+                        sb.append("UPDATE dwm_vlms_one_order_to_end e JOIN dim_vlms_warehouse_rs a on a.WAREHOUSE_CODE = '" + in_warehouse_code +
+                                "' JOIN dwm_vlms_sptb02 s on e.VIN = s.VVIN SET e.IN_DISTRIBUTE_TIME = " + sample_u_t_c +
+                                " , e.WAREHOUSE_UPDATETIME = " + nowTime + " WHERE e.VIN = '" + vin + "'  AND e.LEAVE_FACTORY_TIME < " + sample_u_t_c + " AND a.WAREHOUSE_TYPE = 'T2' AND s.VYSFS = 'G' "
+                                + " AND e.IN_SITE_TIME < " + sample_u_t_c + ";");
+                    }
+            }
+
         }
 
         if (sb.length() > 0) {
