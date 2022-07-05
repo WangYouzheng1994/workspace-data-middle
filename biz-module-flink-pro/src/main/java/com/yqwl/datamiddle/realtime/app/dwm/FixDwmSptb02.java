1e6b10ab7bf5ae193ec8d53e095e414007b5cccb
xiaofeng1 <bdffgee@gmail.com>
2022-07-05 22:35:41 +0800

    <一单到底>修改为线上的库名称

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/FixDwmSptb02.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/FixDwmSptb02.java
@@ -65,8 +65,8 @@ public class FixDwmSptb02 {
                 .hostname(props.getStr("cdc.mysql.hostname"))
                 .port(props.getInt("cdc.mysql.port"))
                 .databaseList(StrUtil.getStrList(props.getStr("cdc.mysql.database.list"), ","))
-                //.tableList("data_flink.ods_vlms_sptb02")
-                .tableList("data_middle_flink.ods_vlms_sptb02")
+                .tableList("data_flink.ods_vlms_sptb02")
+                //.tableList("data_middle_flink.ods_vlms_sptb02")
                 .username(props.getStr("cdc.mysql.username"))
                 .password(props.getStr("cdc.mysql.password"))
                 .deserializer(new CustomerDeserialization()) // converts SourceRecord to JSON String
