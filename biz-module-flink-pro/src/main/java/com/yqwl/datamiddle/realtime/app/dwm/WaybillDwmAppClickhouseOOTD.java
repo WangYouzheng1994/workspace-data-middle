46ab1023b1e66a1250dc22bb7ba4bd33f116c14e
xiaofeng1 <bdffgee@gmail.com>
2022-07-13 11:00:29 +0800

    修改类名字使其通俗易懂

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/WaybillDwmAppClickhouseOOTD.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/WaybillDwmAppClickhouseOOTD.java
@@ -29,7 +29,7 @@ import java.util.concurrent.TimeUnit;
  * @Version: V1.0
  */
 @Slf4j
-public class WaybillDwmAppOOTD {
+public class WaybillDwmAppClickhouseOOTD {
 
     public static void main(String[] args) throws Exception {
         //1.创建环境  Flink 流式处理环境
