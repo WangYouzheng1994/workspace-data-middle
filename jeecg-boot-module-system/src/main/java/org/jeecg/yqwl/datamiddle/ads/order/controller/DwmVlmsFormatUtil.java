631e68b5a5948b48cc3afe891dc6be4cc4aa1c87
qingsong <qingsong@qq.com>
2022-07-19 11:40:02 +0800

    公共工具类。

---
+++ jeecg-boot-module-system/src/main/java/org/jeecg/yqwl/datamiddle/ads/order/controller/DwmVlmsFormatUtil.java
@@ -0,0 +1,10 @@
+package org.jeecg.yqwl.datamiddle.ads.order.controller;
+
+/**
+ * @Description: 公共工具类
+ * @Author: WangYouzheng
+ * @Date: 2022/7/19 11:39
+ * @Version: V1.0
+ */
+public class DwmVlmsFormatUtil {
+}
