024729d9114f374bf725c222e74f32977c8dcc12
xiaofeng1 <bdffgee@gmail.com>
2022-07-19 21:05:23 +0800

    字段更改名字从is_g改为type_g

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/bean/DwmOneOrderToEnd.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/bean/DwmOneOrderToEnd.java
@@ -414,17 +414,17 @@ public class DwmOneOrderToEnd implements Serializable {
     /**
      * 是否为公路的运输方式 1为是 0为否 20220719添加
      */
-    private Integer IS_G;
+    private Integer TYPE_G;
 
     /**
      * 是否为铁路的运输方式 1为是 0为否 20220719添加
      */
-    private Integer IS_T;
+    private Integer TYPE_T;
 
     /**
      * 是否为水路的运输方式 1为是 0为否 20220719添加
      */
-    private Integer IS_S;
+    private Integer TYPE_S;
 
 
 }
