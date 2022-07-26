4be6a9d7556e02b049658aa68028cad2a2b5669c
xiaofeng1 <bdffgee@gmail.com>
2022-07-26 16:51:12 +0800

    <一单到底> fix bug for 8 timeNode of the waterway;

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/func/SimpleBaseStationDataSink.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/func/SimpleBaseStationDataSink.java
@@ -27,6 +27,7 @@ public class SimpleBaseStationDataSink<T> extends RichSinkFunction<DwdBaseStatio
         String vin = data.getVIN();
         String wlckdm = data.getPHYSICAL_CODE();
         String warehouse_type = data.getWAREHOUSE_TYPE();
+        String shop_no = data.getSHOP_NO();
         String physical_code = data.getPHYSICAL_CODE();
         String operate_type = data.getOPERATE_TYPE();
 
@@ -34,8 +35,7 @@ public class SimpleBaseStationDataSink<T> extends RichSinkFunction<DwdBaseStatio
 
         //==============================================处理铁路运单=============================================================//
         //1.查询铁路运单 根据仓库代码 vvin码定位一条记录 ,每一个站台都会有两个时间，入站台时间和出站台时间
-        // 查询开始站台的运单记录
-        // 当前查询到记录值为空，从来没有被更新过
+        //  1.1 处理入 开始站台时间 (集站时间)
         if (StringUtils.isNotBlank(data.getVIN())) {
             if (IN_STORE.equals(operateType)) {
             /* String inSql = "UPDATE dwm_vlms_sptb02 SET IN_START_PLATFORM_TIME=" + data.getSAMPLE_U_T_C() + " WHERE VYSFS IN ('L1', 'T') AND START_PHYSICAL_CODE='"
@@ -63,7 +63,7 @@ public class SimpleBaseStationDataSink<T> extends RichSinkFunction<DwdBaseStatio
                 sb.append("UPDATE dwm_vlms_sptb02 SET IN_END_PLATFORM_TIME=" + data.getSAMPLE_U_T_C() + " WHERE VYSFS IN ('L1', 'T') AND END_PHYSICAL_CODE='"
                         + wlckdm + "' AND VVIN='" + vin + "' AND ( IN_END_PLATFORM_TIME = 0 OR IN_END_PLATFORM_TIME > " + data.getSAMPLE_U_T_C() + " );");
             }
-            //处理出目的站台时间
+            // 1.4 处理出目的站台时间
             if (IN_STORE.equals(operateType)) {
             /* String inSql = "UPDATE dwm_vlms_sptb02 SET UNLOAD_RAILWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE VYSFS in ('L1', 'T') AND END_PHYSICAL_CODE='"
                     + wlckdm + "' AND VVIN='" + vin + "' AND ( UNLOAD_RAILWAY_TIME = 0 OR UNLOAD_RAILWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
@@ -73,39 +73,58 @@ public class SimpleBaseStationDataSink<T> extends RichSinkFunction<DwdBaseStatio
             }
 
             //==============================================处理水路运单运单=============================================================//
-            // 查询开始站台的运单记录
-            //当前查询到记录值为空，从来没有被更新过
+            // 1.1 处理入 开始站台时间 (集港时间)
             if (IN_STORE.equals(operateType)) {
             /* String inSql = "UPDATE dwm_vlms_sptb02 SET IN_START_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND START_PHYSICAL_CODE='"
                     + wlckdm + "' AND VVIN='" + vin + "' AND ( IN_START_WATERWAY_TIME = 0 OR IN_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql); */
-                sb.append("UPDATE dwm_vlms_sptb02 SET IN_START_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND START_PHYSICAL_CODE='"
-                        + wlckdm + "' AND VVIN='" + vin + "' AND ( IN_START_WATERWAY_TIME = 0 OR IN_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " );");
+                sb.append(" UPDATE dwm_vlms_sptb02 d1 " +
+                          " JOIN dim_vlms_warehouse_rs d2 ON d1.VVIN = '" + vin
+                        + "' AND d2.WAREHOUSE_CODE = '" + shop_no
+                        + "' AND d1.START_PHYSICAL_CODE = d2.VWLCKDM AND d2.WAREHOUSE_TYPE = 'T3' AND d1.TRAFFIC_TYPE = 'S' "
+                        + " AND ( d1.IN_START_WATERWAY_TIME = 0 OR d1.IN_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " ) "
+                        + " SET d1.IN_START_WATERWAY_TIME= " + data.getSAMPLE_U_T_C() + " ;"
+                );
             }
-            // 1.2 处理开始站台的出站台时间
+            // 1.2 处理出 开始站台时间
             if (OUT_STOCK.equals(operateType)) {
             /* String inSql = "UPDATE dwm_vlms_sptb02 SET END_START_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND START_PHYSICAL_CODE='"
                     + wlckdm + "' AND VVIN='" + vin + "' AND ( END_START_WATERWAY_TIME = 0 OR END_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql); */
-                sb.append("UPDATE dwm_vlms_sptb02 SET END_START_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND START_PHYSICAL_CODE='"
-                        + wlckdm + "' AND VVIN='" + vin + "' AND ( END_START_WATERWAY_TIME = 0 OR END_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " );");
+                sb.append(" UPDATE dwm_vlms_sptb02 d1 " +
+                        " JOIN dim_vlms_warehouse_rs d2 ON d1.VVIN = '" + vin
+                        + "' AND d2.WAREHOUSE_CODE = '" + shop_no
+                        + "' AND d1.START_PHYSICAL_CODE = d2.VWLCKDM AND d2.WAREHOUSE_TYPE = 'T3' AND d1.TRAFFIC_TYPE = 'S' "
+                        + " AND ( d1.END_START_WATERWAY_TIME = 0 OR d1.END_START_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " ) "
+                        + " SET d1.END_START_WATERWAY_TIME= " + data.getSAMPLE_U_T_C() + " ;"
+                );
             }
 
-            //1.3 处理目的站台的入站台时间和出站台时间
+            //1.3 处理目的站台的入站台时间
             if (IN_STORE.equals(operateType)) {
             /* String inSql = "UPDATE dwm_vlms_sptb02 SET IN_END_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND END_PHYSICAL_CODE='"
                     + wlckdm + "' AND VVIN='" + vin + "' AND ( IN_END_WATERWAY_TIME = 0 OR IN_END_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql); */
-                sb.append("UPDATE dwm_vlms_sptb02 SET IN_END_WATERWAY_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND END_PHYSICAL_CODE='"
-                        + wlckdm + "' AND VVIN='" + vin + "' AND ( IN_END_WATERWAY_TIME = 0 OR IN_END_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " );");
+                sb.append(" UPDATE dwm_vlms_sptb02 d1 " +
+                        " JOIN dim_vlms_warehouse_rs d2 ON d1.VVIN = '" + vin
+                        + "' AND d2.WAREHOUSE_CODE = '" + shop_no
+                        + "' AND d1.END_PHYSICAL_CODE = d2.VWLCKDM AND d2.WAREHOUSE_TYPE = 'T3' AND d1.TRAFFIC_TYPE = 'S' "
+                        + " AND ( IN_END_WATERWAY_TIME = 0 OR IN_END_WATERWAY_TIME > " + data.getSAMPLE_U_T_C() + " ) "
+                        + " SET d1.IN_END_WATERWAY_TIME= " + data.getSAMPLE_U_T_C() + " ;"
+                );
             }
             //处理出目的站台时间
             if (IN_STORE.equals(operateType)) {
             /* String inSql = "UPDATE dwm_vlms_sptb02 SET UNLOAD_SHIP_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND END_PHYSICAL_CODE='"
                     + wlckdm + "' AND VVIN='" + vin + "' AND ( UNLOAD_SHIP_TIME = 0 OR UNLOAD_SHIP_TIME > " + data.getSAMPLE_U_T_C() + " )";
             DbUtil.executeUpdate(inSql); */
-                sb.append("UPDATE dwm_vlms_sptb02 SET UNLOAD_SHIP_TIME=" + data.getSAMPLE_U_T_C() + " WHERE  VYSFS = 'S' AND END_PHYSICAL_CODE='"
-                        + wlckdm + "' AND VVIN='" + vin + "' AND ( UNLOAD_SHIP_TIME = 0 OR UNLOAD_SHIP_TIME > " + data.getSAMPLE_U_T_C() + " );");
+                sb.append(" UPDATE dwm_vlms_sptb02 d1 " +
+                        " JOIN dim_vlms_warehouse_rs d2 ON d1.VVIN = '" + vin
+                        + "' AND d2.WAREHOUSE_CODE = '" + shop_no
+                        + "' AND d1.END_PHYSICAL_CODE = d2.VWLCKDM AND d2.WAREHOUSE_TYPE = 'T3' AND d1.TRAFFIC_TYPE = 'S' "
+                        + " AND ( UNLOAD_SHIP_TIME = 0 OR UNLOAD_SHIP_TIME > " + data.getSAMPLE_U_T_C() + " ) "
+                        + " SET d1.UNLOAD_SHIP_TIME= " + data.getSAMPLE_U_T_C() + " ;"
+                );
             }
             // 2.将库房类型WAREHOUSE_TYPE更新到dwm_sptb02中去  前置条件: 仓库种类不为空,物理仓库代码不为空,vin码不为空,出入库类型为出库.
             if (StringUtils.isNotBlank(warehouse_type) && StringUtils.isNotBlank(physical_code) && StringUtils.isNotBlank(vin) && StringUtils.equals(operate_type, "OutStock")) {
