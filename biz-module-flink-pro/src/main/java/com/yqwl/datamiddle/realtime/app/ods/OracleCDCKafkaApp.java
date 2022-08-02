578954fc896cd772fcf4bc8328e381168710da39
xiaofeng1 <bdffgee@gmail.com>
2022-08-02 21:55:05 +0800

    spi: 开始正式启用修复了八个小时时区和时间戳多0的问题的代码 20220802

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/ods/OracleCDCKafkaApp.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/ods/OracleCDCKafkaApp.java
@@ -91,8 +91,8 @@ public class OracleCDCKafkaApp {
         properties.put("decimal.handling.mode", "string");   //解决number类数据 不能解析的方法
         properties.put("event.processing.failure.handling.mode", "warn");
         properties.put("rac.nodes","10.123.175.197:1250,10.123.175.197:1251");
-        //properties.put("converters", "aaa");
-        //properties.put("aaa.type", "com.yqwl.datamiddle.realtime.util.TimestampConverter");
+        properties.put("converters", "aaa");
+        properties.put("aaa.type", "com.yqwl.datamiddle.realtime.util.TimestampConverter");
         //properties.put("database.serverTimezone", "UTC");
         //properties.put("database.serverTimezone", "Asia/Shanghai");
         properties.put("database.url", "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(LOAD_BALANCE=OFF)(FAILOVER=OFF)(ADDRESS=(PROTOCOL=tcp)(HOST=" +
