5b77f3e4407e171c1dfc5d5ab353acc6f47eda7b
xiaofeng1 <bdffgee@gmail.com>
2022-10-21 10:53:15 +0800

    修改为增量

--- biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/WaybillDwmAppClickhouseOOTD.java
+++ biz-module-flink-pro/src/main/java/com/yqwl/datamiddle/realtime/app/dwm/WaybillDwmAppClickhouseOOTD.java
@@ -67,7 +67,7 @@ public class WaybillDwmAppClickhouseOOTD {
                 .password(props.getStr("cdc.mysql.password"))
                 .deserializer(new CustomerDeserialization()) // converts SourceRecord to JSON String
                 .debeziumProperties(properties)
-                .startupOptions(StartupOptions.initial())
+                .startupOptions(StartupOptions.latest())
                 .distributionFactorUpper(10.0d)  // 针对cdc的错误算法的更改
                 .serverId("5413-5416")
                 .build();
